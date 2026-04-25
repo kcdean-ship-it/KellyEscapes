905a68ddeaba22b6a23691cc18b8ac3137200f5d
Kelly Dean <kcdean@seu.edu>
2026-04-25 16:45:51 -0400

    created classes

--- game.py
+++ game.py
@@ -1,1 +1,13 @@
-print("Welcome to my Escape Game!")
+# print("Welcome to my Escape Game!")
+
+class Room:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+class Item:
+    def __init__(self, name, description, is_moveable):
+        self.name = name
+        self.description = description
+        self.is_moveable = is_moveable
+
